aa32d045a083660004841fc19272d01454769d42
Brady Giacopelli <110193766+bradygiacopelli@users.noreply.github.com>
2024-11-21 18:55:03 -0600

    forum styling

--- server/models/Forum.js
+++ server/models/Forum.js
@@ -5,6 +5,7 @@ const forumSchema = new mongoose.Schema({
     description: { type: String },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Users subscribed to this forum
+    image: { type: String, default: '/uploads/default-forum-image.png' }, // Default image
     createdAt: { type: Date, default: Date.now },
 });
 
